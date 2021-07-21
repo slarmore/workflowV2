6f3a8ddbb8b6423c9cafd7a91cd2940b4a882cb7
neal-p <50751083+lovesSubPcs@users.noreply.github.com>
2021-07-21 16:12:44 -0400

    reorganized_ammended

---
+++ examples/badmol_example.py
@@ -0,0 +1,15 @@
+from workflow import molecule
+from workflow.gaussian import GAUSSIAN
+from workflow.calculator import Run
+
+mol = molecule.XYZToMol('badmol.xyz')
+
+calculator = GAUSSIAN(mol,runtype='opt_freq',nproc=16,mem=120,jobname='opt_badmol-{0}'.format(tries),opt='calcfc,ts,noeigen,maxstep=10',method='b3lyp/6-31G(d)',empiricaldispersion='GD3BJ',scrf='iefpcm,solvent=water',scf='xqc')
+
+mol = Run(calculator)
+
+print('Optimized the geometry successfully')
+
+print('test')
+print('test again')
+print('test a third')
