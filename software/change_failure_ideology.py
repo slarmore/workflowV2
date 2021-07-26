418c761fbac3a06e9331c9c2af6d8557f14e5b3d
neal-p <50751083+lovesSubPcs@users.noreply.github.com>
2021-07-25 23:39:56 -0400

    add self to fix_errors

--- software/change_failure_ideology.py
+++ software/change_failure_ideology.py
@@ -263,7 +263,7 @@ class gaussian:
         return(calculator.mol)
 
 
-    def fix_errors(mol,kwargs):
+    def fix_errors(self,mol,kwargs):
 
         #not very elegant, but loop through the list and 
         #add keywords to fix any known errors
