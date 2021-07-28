d991da322b651e6f473fd1792d3b32683f06217b
neal-p <58431101+neal-p@users.noreply.github.com>
2021-07-28 10:40:39 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='workflowV2',
+      version='2.0',
+      description='QM workflow python package',
+      author='Patrick Neal',
+      author_email='prnmac12@gmail.com',
+      url='https://github.com/neal-p/workflowV2/',
+      packages=['workflowV2'],
+     )
