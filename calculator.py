ef227ff4f519c42b6c6b0e8716912ed3935a8e4f
neal-p <50751083+lovesSubPcs@users.noreply.github.com>
2021-08-04 17:03:01 -0400

    same as before

--- calculator.py
+++ calculator.py
@@ -168,7 +168,7 @@ def RunBatch(calculators,jobname='batch_job',max=50,tries=1):
             with open(jobname + '/' + jobname+ '.sbatch','w') as sbatch:
                 sbatch.write(generic_batch(jobname,calculators,max,need_resub))
 
-            for index in enumerate(need_resub):
+            for index in need_resub:
                 command_file_name = '{0}/{0}-{1}-try{2}.sh'.format(jobname,index+1,calculators[index].try_count)   #slurm array index start at 1 not 0
                 with open(command_file_name,'w') as command_file:
                     command_file.write(generic_command(calculator))
