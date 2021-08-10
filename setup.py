c4ae52c0a8b5726e303f88188a5f59cca8cc30fb
neal-p <58431101+neal-p@users.noreply.github.com>
2021-08-09 23:08:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ from setuptools import setup,find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-def checkdependencies(
-
 setup(
     cmdclass={'check_dependencies':checkdependencies}
     name = "workflowV2",
