ff59387692a856afaa190bc58bf1cf28a0b1afdb
Patrick Neal <p.neal.chem@gmail.com>
2021-08-29 23:27:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "workflowV2",
-    version = "2.221",
+    version = "2.2211",
     author = "Patrick Neal",
     author_email = "prnmac12@gmail.com",
     description = ("An automation tool for QM calculations"),
