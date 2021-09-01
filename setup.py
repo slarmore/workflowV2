752cb77ea446b6441f99d2f1a19641a5119cf98f
Patrick Neal <p.neal.chem@gmail.com>
2021-08-31 21:17:04 -0400

    Update setup.py
    Count RDkit conformers accurately

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "workflowV2",
-    version = "2.2222",
+    version = "2.2223",
     author = "Patrick Neal",
     author_email = "prnmac12@gmail.com",
     description = ("An automation tool for QM calculations"),
