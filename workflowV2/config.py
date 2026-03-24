71c1dfeb707e1e9985308a81347bfeb8454c6965
slarmore <153005846+slarmore@users.noreply.github.com>
2026-03-24 14:42:50 -0400

    Update config.py

--- workflowV2/config.py
+++ workflowV2/config.py
@@ -1,10 +1,10 @@
 #EXE FILES - required to run a calculation with the program
-crest_exe = '/work/lopez/xtb/crest'
-xtb_exe = '/work/lopez/xtb/xtb_6.2.3/bin/xtb'
-g16root = '/work/lopez/'
+crest_exe = '/projects/lopez/share_from_Sean/software/xtb-6.7.1/crest'
+xtb_exe = '/projects/lopez/share_from_Sean/software/xtb-6.7.1/bin'
+g16root = '/projects/lopez/'
 
 
 #SLURM SETTINGS
-default_partition = 'short'
+default_partition = 'lopez'
 default_time = '1-00:00:00'
 
